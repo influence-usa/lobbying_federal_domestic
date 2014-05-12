eaa37c223ab3cf3763ce7e35fb7e6d3d5a354af9
Bob Lannon <blannon@gmail.com>
2014-05-12 18:26:59 -0400

    refactored extract and added house_xml command

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,4 +1,4 @@
 import log
 from download import download_sopr, download_house_xml
-from extract import extract_sopr
+from extract import extract_sopr, extract_house_xml
 from transform import transform_sopr
--- tasks/extract.py
+++ tasks/extract.py
@@ -3,8 +3,7 @@ import logging
 import zipfile
 from glob import glob
 
-from stream import ThreadPool
-from stream import map, filter
+from multiprocessing.dummy import Pool as ThreadPool
 
 from settings import CACHE_DIR, ORIG_DIR
 from .utils import mkdir_p, translate_dir
@@ -13,20 +12,55 @@ from .log import set_up_logging
 log = set_up_logging('extract', loglevel=logging.DEBUG)
 
 
+def log_result(result):
+    if result[0] == 'success':
+        src_dir, dest_dir, num_files = result[1:]
+        log.info("successfully extracted " +
+                 "{src_dir} => {dest_dir} ({num} files)".format(
+                     src_dir=src_dir, dest_dir=dest_dir, num=num_files))
+    elif result[0] == 'failure':
+        loc, e = result[1:]
+        log.error("extracting from {loc} failed: {exception}".format(
+            loc=loc, exception=str(e)))
+    else:
+        raise Exception('Result for {loc} was neither success nor failure?')
+
+
 def check_ext(path, ext=None):
     return os.path.splitext(path)[1] == ext
 
 
-def extract_zip(path, destination_dir):
-    with zipfile.ZipFile(path, "r") as z:
-        num_files = len(z.namelist())
-        z.extractall(destination_dir)
-        return (path, destination_dir, num_files)
+def extract_zip(cache_path):
+    old_path, new_path = translate_dir(cache_path, from_dir=CACHE_DIR,
+                                       to_dir=ORIG_DIR)
+    try:
+        with zipfile.ZipFile(old_path, "r") as z:
+            num_files = len(z.namelist())
+            z.extractall(new_path)
+            return ('success', old_path, new_path, num_files)
+    except Exception as e:
+        return ('failure', old_path, e)
 
 
-def extract_all_zips(path_dest_pairs):
-    for path, destination_dir in path_dest_pairs:
-        yield extract_zip(path, destination_dir)
+def extract_all_zips(cache_paths, options):
+    threaded = options.get('threaded', False)
+    thread_num = options.get('thread_num', 4)
+
+    if threaded:
+        pool = ThreadPool(thread_num)
+        for path in cache_paths:
+            if check_ext(path, ext='.zip'):
+                pool.apply_async(extract_zip, args=(path,),
+                                 callback=log_result)
+            else:
+                raise Exception("{} not a zip file!".format(path))
+        pool.close()
+        pool.join()
+    else:
+        for path in cache_paths:
+            old_path, new_path = translate_dir(path, from_dir=CACHE_DIR,
+                                               to_dir=ORIG_DIR)
+            log_result(extract_zip(old_path, new_path))
 
 
 def extract_sopr(options):
@@ -40,17 +74,18 @@ def extract_sopr(options):
     log.debug("cache paths ({num}):".format(num=len(cache_paths)) +
               "\n\t".join(cache_paths))
 
-    extracted = cache_paths >> filter(lambda x: check_ext(x, ext='.zip')) \
-                            >> map(lambda p: translate_dir(p,
-                                                           from_dir=CACHE_DIR,
-                                                           to_dir=ORIG_DIR)) \
-                            >> ThreadPool(extract_all_zips)
+    extract_all_zips(cache_paths, options)
 
-    for path, destination_dir, num_files in extracted:
-        log.info("successfully extracted " +
-                 "{path} to {dest_dir} ({num} files)".format(
-                    path=path, dest_dir=destination_dir, num=num_files))
 
-    for url, exception in extracted.failure:
-        log.error("extracting from {path} failed: {exception}".format(
-            path=url, exception=exception))
+def extract_house_xml(options):
+    if not os.path.exists(ORIG_DIR):
+        mkdir_p(ORIG_DIR)
+
+    if options.get('loglevel', None):
+        log.setLevel(options['loglevel'])
+
+    cache_paths = glob(os.path.join(CACHE_DIR, 'house_xml/*/*/*/*.zip'))
+    log.debug("cache paths ({num}):".format(num=len(cache_paths)) +
+              "\n\t".join(cache_paths))
+
+    extract_all_zips(cache_paths, options)
