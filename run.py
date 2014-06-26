4e87a18b3a24e76f98c75cc24ac83d510e07ff53
Bob Lannon <blannon@gmail.com>
2014-06-26 19:20:02 -0400

    added sopr_html task to run

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ loglevels = {'info': logging.INFO,
              'warn': logging.WARN}
 
 possible_actions = ['download', 'extract', 'transform']
-data_types = ['sopr_xml', 'house_xml', 'sopr_report_types']
+data_types = ['sopr_html', 'sopr_xml', 'house_xml', 'sopr_report_types']
 
 parser = argparse.ArgumentParser(description='Run scripts')
 
@@ -21,6 +21,7 @@ parser.add_argument('action', choices=possible_actions)
 parser.add_argument('data_type', choices=data_types)
 parser.add_argument('--force', action='store_true')
 parser.add_argument('--threaded', action='store_true')
+parser.add_argument('--backfill', action='store_true')
 parser.add_argument('--thread_num', action='store', type=int)
 parser.add_argument('--loglevel', default='debug')
 
--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,5 +1,5 @@
 import log
-from download import download_sopr_xml, download_house_xml,\
-    download_sopr_report_types
+from download import download_sopr_xml, download_sopr_html,\
+    download_house_xml, download_sopr_report_types
 from extract import extract_sopr_xml, extract_house_xml
 from transform import transform_sopr_xml, transform_house_xml
