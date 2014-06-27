3664413b9c5ad1ee921d52524ff9b51ecba5b24d
Zack Maril <zmaril@sunlightfoundation.com>
2014-06-27 15:15:41 +0000

    Spell checking isn't useful yet.

--- dedupe/graph.py
+++ dedupe/graph.py
@@ -1,6 +1,5 @@
 from being import groupMerge, countTypes, matchTypeAndHasFields
 from load import loadData
-from norvig import correctSentence
 from pprint import pprint
 from save import steralize, save, project
 from text import extractNames
@@ -8,13 +7,6 @@ from text import extractNames
 def main():
     print("Loading universe...")
     universe = loadData()
-    model = {k : float("inf") for k in ["corporation","company","corporations",
-                                        "associates","association","associated",
-                                        "companies","incorporated","associations",
-                                        "national","china","rational"]}
-
-    def mnf(f):
-        return lambda v: extractNames(correctSentence(model,v[f].lower()))
 
     groupMerge(universe,
                matchTypeAndHasFields("client",["name"]),               
@@ -31,16 +23,7 @@ def main():
                lambda v: p(v) and v["state"] not in ["DC","VA","MD"] and v["city"] != "DC",
                lambda v: [(v["address"],v["city"],v["country"],v["state"],v["zip"])],
                description="Merging clients based on exact matching of address fields (sans DC area)")
-    
-    #Spell checking names is slow, makes some mistakes without a full
-    #list of words and only resolves ~32 entities. Not worth the time
-    #at the moment.
-    
-    # groupMerge(universe,
-    #            matchTypeAndHasFields("client",["name"]),               
-    #            mnf("name"),
-    #            description="Merged clients based on extracted, cleaned and *corrected* name")
-        
+            
     # groupMerge(universe,
     #            matchTypeAndHasFields("firm",["orgname"]),
     #            mnf("orgname"),
