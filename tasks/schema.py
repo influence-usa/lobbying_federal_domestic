886f0a01c6145ca6bceba2469240ce193d932baa
Bob Lannon <blannon@gmail.com>
2014-07-10 00:31:51 -0400

    renamed percentage, changed question values to strings

--- tasks/schema.py
+++ tasks/schema.py
@@ -123,7 +123,7 @@ ld1_schema = [
     },
     {
         'section': 'datetimes',
-        'lda_question': 1,
+        'lda_question': '1',
         'field': 'effective_date',
         'path': '/html/body/table[2]/tbody/tr[1]/td[3]/div',
         'parser': parse_datetime
@@ -158,119 +158,119 @@ ld1_schema = [
     },
     {
         'section': 'registrant',
-        'lda_question': 3,
+        'lda_question': '3',
         'field': 'registrant_name',
         'path': '/html/body/table[3]/tbody/tr/td[3]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 3,
+        'lda_question': '3',
         'field': 'registrant_address_one',
         'path': '/html/body/table[4]/tbody/tr/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 3,
+        'lda_question': '3',
         'field': 'registrant_address_two',
         'path': '/html/body/table[4]/tbody/tr/td[4]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 3,
+        'lda_question': '3',
         'field': 'registrant_city',
         'path': '/html/body/table[5]/tbody/tr/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 3,
+        'lda_question': '3',
         'field': 'registrant_state',
         'path': '/html/body/table[5]/tbody/tr/td[4]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 3,
+        'lda_question': '3',
         'field': 'registrant_zip',
         'path': '/html/body/table[5]/tbody/tr/td[6]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 3,
+        'lda_question': '3',
         'field': 'registrant_country',
         'path': '/html/body/table[5]/tbody/tr/td[8]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 4,
+        'lda_question': '4',
         'field': 'registrant_ppb_city',
         'path': '/html/body/table[6]/tbody/tr/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 4,
+        'lda_question': '4',
         'field': 'registrant_ppb_state',
         'path': '/html/body/table[6]/tbody/tr/td[4]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 4,
+        'lda_question': '4',
         'field': 'registrant_ppb_zip',
         'path': '/html/body/table[6]/tbody/tr/td[6]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 4,
+        'lda_question': '4',
         'field': 'registrant_ppb_country',
         'path': '/html/body/table[6]/tbody/tr/td[8]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 5,
+        'lda_question': '5',
         'field': 'registrant_international_phone',
         'path': '/html/body/table[7]/tbody/tr/td[2]/input',
         'parser': checkbox_boolean
     },
     {
         'section': 'registrant',
-        'lda_question': 5,
+        'lda_question': '5',
         'field': 'registrant_contact',
         'path': '/html/body/table[8]/tbody/tr/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 5,
+        'lda_question': '5',
         'field': 'registrant_phone',
         'path': '/html/body/table[8]/tbody/tr/td[4]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 5,
+        'lda_question': '5',
         'field': 'registrant_email',
         'path': '/html/body/table[8]/tbody/tr/td[6]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 5,
+        'lda_question': '5',
         'field': 'registrant_email',
         'path': '/html/body/table[8]/tbody/tr/td[6]/div',
         'parser': clean_text
     },
     {
         'section': 'registrant',
-        'lda_question': 6,
+        'lda_question': '6',
         'field': 'registrant_general_description',
         'path': '/html/body/div[2]',
         'parser': clean_text
@@ -284,112 +284,112 @@ ld1_schema = [
     },
     {
         'section': 'client',
-        'lda_question': 7,
+        'lda_question': '7',
         'field': 'client_name',
         'path': '/html/body/table[9]/tbody/tr[1]/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 7,
+        'lda_question': '7',
         'field': 'client_address',
         'path': '/html/body/table[9]/tbody/tr[2]/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 7,
+        'lda_question': '7',
         'field': 'client_city',
         'path': '/html/body/table[10]/tbody/tr/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 7,
+        'lda_question': '7',
         'field': 'client_state',
         'path': '/html/body/table[10]/tbody/tr/td[4]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 7,
+        'lda_question': '7',
         'field': 'client_zip',
         'path': '/html/body/table[10]/tbody/tr/td[6]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 7,
+        'lda_question': '7',
         'field': 'client_country',
         'path': '/html/body/table[10]/tbody/tr/td[8]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 8,
+        'lda_question': '8',
         'field': 'client_ppb_city',
         'path': '/html/body/table[11]/tbody/tr/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 8,
+        'lda_question': '8',
         'field': 'client_ppb_state',
         'path': '/html/body/table[11]/tbody/tr/td[4]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 8,
+        'lda_question': '8',
         'field': 'client_ppb_zip',
         'path': '/html/body/table[11]/tbody/tr/td[6]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 8,
+        'lda_question': '8',
         'field': 'client_ppb_country',
         'path': '/html/body/table[11]/tbody/tr/td[8]/div',
         'parser': clean_text
     },
     {
         'section': 'client',
-        'lda_question': 9,
+        'lda_question': '9',
         'field': 'client_general_description',
         'path': '/html/body/div[3]',
         'parser': clean_text
     },
     {
         'section': 'lobbyists',
-        'lda_question': 10,
+        'lda_question': '10',
         'field': 'lobbyists',
         'path': '/html/body/table[12]/tbody/tr[position() > 2]',
         'parser': parse_array,
         'children': [
             {
                 'section': 'lobbyists',
-                'lda_question': 10,
+                'lda_question': '10',
                 'field': 'lobbyist_first_name',
                 'path': 'td[1]',
                 'parser': clean_text
             },
             {
                 'section': 'lobbyists',
-                'lda_question': 10,
+                'lda_question': '10',
                 'field': 'lobbyist_last_name',
                 'path': 'td[2]',
                 'parser': clean_text
             },
             {
                 'section': 'lobbyists',
-                'lda_question': 10,
+                'lda_question': '10',
                 'field': 'lobbyist_suffix',
                 'path': 'td[3]',
                 'parser': clean_text
             },
             {
                 'section': 'lobbyists',
-                'lda_question': 10,
+                'lda_question': '10',
                 'field': 'lobbyist_covered_official_position',
                 'path': 'td[4]',
                 'parser': clean_text
@@ -398,14 +398,14 @@ ld1_schema = [
     },
     {
         'section': 'lobbying_issues',
-        'lda_question': 11,
+        'lda_question': '11',
         'field': 'lobbying_issues',
         'path': '/html/body/table[13]/tbody/tr/td/div',
         'parser': parse_array,
         'children': [
             {
                 'section': 'lobbying_issues',
-                'lda_question': 11,
+                'lda_question': '11',
                 'field': 'issue_code',
                 'path': '.',
                 'parser': clean_text
@@ -414,35 +414,35 @@ ld1_schema = [
     },
     {
         'section': 'lobbying_issues_detail',
-        'lda_question': 12,
+        'lda_question': '12',
         'field': 'lobbying_issues_detail',
         'path': '/html/body/p[10]',
         'parser': clean_text
     },
     {
         'section': 'affiliated_organizations',
-        'lda_question': 13,
+        'lda_question': '13',
         'field': 'affiliated_organizations_no',
         'path': '/html/body/table[14]/tbody/tr/td[1]/input',
         'parser': checkbox_boolean
     },
     {
         'section': 'affiliated_organizations',
-        'lda_question': 13,
+        'lda_question': '13',
         'field': 'affiliated_organizations_yes',
         'path': '/html/body/table[14]/tbody/tr/td[2]/input',
         'parser': checkbox_boolean
     },
     {
         'section': 'affiliated_organizations',
-        'lda_question': 13,
+        'lda_question': '13',
         'field': 'affiliated_organizations_url',
         'path': '/html/body/table[15]/tbody/tr/td[2]/div',
         'parser': clean_text
     },
     {
         'section': 'affiliated_organizations',
-        'lda_question': 13,
+        'lda_question': '13',
         'field': 'affiliated_organizations',
         'path': '/html/body/table[16]/tbody/tr[position() > 3]',
         'parser': parse_even_odd,
@@ -451,21 +451,21 @@ ld1_schema = [
                 'odd': [
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_name',
                         'path': 'td[1]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_address',
                         'path': 'td[2]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_ppb_city',
                         'path': 'td[3]/table/tbody/tr/td[2]/div',
                         'parser': clean_text
@@ -474,42 +474,42 @@ ld1_schema = [
                 'even': [
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_city',
                         'path': 'td[2]/table/tbody/tr/td[1]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_state',
                         'path': 'td[2]/table/tbody/tr/td[2]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_zip',
                         'path': 'td[2]/table/tbody/tr/td[3]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_country',
                         'path': 'td[2]/table/tbody/tr/td[4]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_ppb_state',
                         'path': 'td[3]/table/tbody/tr/td[2]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'affiliated_organizations',
-                        'lda_question': 13,
+                        'lda_question': '13',
                         'field': 'affiliated_organization_ppb_country',
                         'path': 'td[3]/table/tbody/tr/td[4]/div',
                         'parser': clean_text
@@ -519,21 +519,21 @@ ld1_schema = [
     },
     {
         'section': 'foreign_entities',
-        'lda_question': 14,
+        'lda_question': '14',
         'field': 'foreign_entities_no',
         'path': '/html/body/table[17]/tbody/tr/td[1]/input',
         'parser': checkbox_boolean
     },
     {
         'section': 'foreign_entities',
-        'lda_question': 14,
+        'lda_question': '14',
         'field': 'foreign_entities_yes',
         'path': '/html/body/table[17]/tbody/tr/td[3]/input',
         'parser': checkbox_boolean
     },
     {
         'section': 'foreign_entities',
-        'lda_question': 14,
+        'lda_question': '14',
         'field': 'foreign_entities',
         'path': '/html/body/table[19]/tbody/tr',
         'parser': parse_even_odd,
@@ -542,14 +542,14 @@ ld1_schema = [
                 'odd': [
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_address',
                         'path': 'td[2]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_city',
                         'path': 'td[3]/table/tbody/tr/td[2]/div',
                         'parser': clean_text
@@ -558,57 +558,57 @@ ld1_schema = [
                 'even': [
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_name',
                         'path': 'td[1]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_city',
                         'path': 'td[2]/table/tbody/tr/td[2]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_state',
                         'path': 'td[2]/table/tbody/tr/td[3]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_country',
                         'path': 'td[2]/table/tbody/tr/td[4]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_ppb_state',
                         'path': 'td[3]/table/tbody/tr/td[2]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_ppb_country',
                         'path': 'td[3]/table/tbody/tr/td[4]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
+                        'lda_question': '14',
                         'field': 'foreign_entity_amount',
                         'path': 'td[4]/div',
                         'parser': clean_text
                     },
                     {
                         'section': 'foreign_entities',
-                        'lda_question': 14,
-                        'field': 'foreign_entity_amount',
+                        'lda_question': '14',
+                        'field': 'foreign_entity_ownership_percentage',
                         'path': 'td[5]/div',
                         'parser': clean_text
                     }
