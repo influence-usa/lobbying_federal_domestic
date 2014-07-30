95d5730902cbd9564d42e9118abfc2d7315be404
Bob Lannon <blannon@gmail.com>
2014-07-30 16:27:04 -0400

    relocated settings

---
+++ settings/local_settings.example.py
@@ -0,0 +1,13 @@
+SECRET = {}
+
+SECRET['AWS_KEY'] = ''
+SECRET['AWS_SECRET'] = ''
+SECRET['SERVE_REMOTE'] = False
+SECRET['BUCKET_CNAME'] = False
+
+BULKDATA = {
+    'AWS_KEY': SECRET['AWS_KEY'],
+    'AWS_SECRET': SECRET['AWS_SECRET'],
+    'AWS_BUCKET': '',
+    'AWS_PREFIX': '',
+}
